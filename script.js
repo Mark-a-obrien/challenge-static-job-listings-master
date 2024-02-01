21cf8e500618e3c3f704f91c7f22d5320cd6b22c
Mark-a-obrien <mark.a.obrien00@gmail.com>
2024-02-01 14:40:27 +0400

    fetch data from json
    Can now fetch data from data.json file

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // vars
 const filterMenuSectionElement = document.querySelector(".filter-menu");
 let currentlyAppliedFilter = [];
+let data;
 
 // Create elemenets 
 function createElements(data) {
@@ -295,11 +296,14 @@ function addElemenetToFilterMenu(filter) {
 
 
 // Initial dispaly 
-function displayWithoutFilter() {
+function displayWithoutFilter(d) {
+
+  data = d
   document.querySelector("main").replaceChildren(filterMenuSectionElement); // remove content in main
 
   for (let i = 0; i < data.length; i++) {
       createElements(data[i]);
+      console.log();
   }
 }
 
@@ -317,17 +321,6 @@ function filterData(allFilters) {
 
     let filterPoints = 0;
     let filterPointsToPass = 0;
-
-
-    // let role = "Backend";
-    // let level = "Junior";
-    // let languages = "Ruby";
-    // let tools = "RoR";
-
-    // let role = "Fullstack";
-    // let level = "Midweight";
-    // let languages = ["JavaScript", "Ruby"];
-    // let tools = ["Sass"];
 
     
     let role = "";
@@ -424,7 +417,7 @@ document.querySelector(".btn-clear").addEventListener("click", () => {
   const filtersSectionElement  = document.querySelector(".filters-section");
 
 
-  displayWithoutFilter();
+  displayWithoutFilter(data);
   currentlyAppliedFilter = [];  
   filtersSectionElement.replaceChildren();
   toggleDispalyOfFilterMenu();
@@ -432,164 +425,169 @@ document.querySelector(".btn-clear").addEventListener("click", () => {
 
 
 
+// Gets json dat form data.json file 
+async function getData() {
+  const response = await fetch('./data.json');
+  const data = await response.json();
+  console.log(data);
 
+  displayWithoutFilter(data);
+}
 
-
-
-
-const data = [
-  {
-    "id": 1,
-    "company": "Photosnap",
-    "logo": "./images/photosnap.svg",
-    "new": true,
-    "featured": true,
-    "position": "Senior Frontend Developer",
-    "role": "Frontend",
-    "level": "Senior",
-    "postedAt": "1d ago",
-    "contract": "Full Time",
-    "location": "USA Only",
-    "languages": ["HTML", "CSS", "JavaScript"],
-    "tools": []
-  },
-  {
-    "id": 2,
-    "company": "Manage",
-    "logo": "./images/manage.svg",
-    "new": true,
-    "featured": true,
-    "position": "Fullstack Developer",
-    "role": "Fullstack",
-    "level": "Midweight",
-    "postedAt": "1d ago",
-    "contract": "Part Time",
-    "location": "Remote",
-    "languages": ["Python"],
-    "tools": ["React"]
-  },
-  {
-    "id": 3,
-    "company": "Account",
-    "logo": "./images/account.svg",
-    "new": true,
-    "featured": false,
-    "position": "Junior Frontend Developer",
-    "role": "Frontend",
-    "level": "Junior",
-    "postedAt": "2d ago",
-    "contract": "Part Time",
-    "location": "USA Only",
-    "languages": ["JavaScript"],
-    "tools": ["React", "Sass"]
-  },
-  {
-    "id": 4,
-    "company": "MyHome",
-    "logo": "./images/myhome.svg",
-    "new": false,
-    "featured": false,
-    "position": "Junior Frontend Developer",
-    "role": "Frontend",
-    "level": "Junior",
-    "postedAt": "5d ago",
-    "contract": "Contract",
-    "location": "USA Only",
-    "languages": ["CSS", "JavaScript"],
-    "tools": []
-  },
-  {
-    "id": 5,
-    "company": "Loop Studios",
-    "logo": "./images/loop-studios.svg",
-    "new": false,
-    "featured": false,
-    "position": "Software Engineer",
-    "role": "Fullstack",
-    "level": "Midweight",
-    "postedAt": "1w ago",
-    "contract": "Full Time",
-    "location": "Worldwide",
-    "languages": ["JavaScript", "Ruby"],
-    "tools": ["Sass"]
-  },
-  {
-    "id": 6,
-    "company": "FaceIt",
-    "logo": "./images/faceit.svg",
-    "new": false,
-    "featured": false,
-    "position": "Junior Backend Developer",
-    "role": "Backend",
-    "level": "Junior",
-    "postedAt": "2w ago",
-    "contract": "Full Time",
-    "location": "UK Only",
-    "languages": ["Ruby"],
-    "tools": ["RoR"]
-  },
-  {
-    "id": 7,
-    "company": "Shortly",
-    "logo": "./images/shortly.svg",
-    "new": false,
-    "featured": false,
-    "position": "Junior Developer",
-    "role": "Frontend",
-    "level": "Junior",
-    "postedAt": "2w ago",
-    "contract": "Full Time",
-    "location": "Worldwide",
-    "languages": ["HTML", "JavaScript"],
-    "tools": ["Sass"]
-  },
-  {
-    "id": 8,
-    "company": "Insure",
-    "logo": "./images/insure.svg",
-    "new": false,
-    "featured": false,
-    "position": "Junior Frontend Developer",
-    "role": "Frontend",
-    "level": "Junior",
-    "postedAt": "2w ago",
-    "contract": "Full Time",
-    "location": "USA Only",
-    "languages": ["JavaScript"],
-    "tools": ["Vue", "Sass"]
-  },
-  {
-    "id": 9,
-    "company": "Eyecam Co.",
-    "logo": "./images/eyecam-co.svg",
-    "new": false,
-    "featured": false,
-    "position": "Full Stack Engineer",
-    "role": "Fullstack",
-    "level": "Midweight",
-    "postedAt": "3w ago",
-    "contract": "Full Time",
-    "location": "Worldwide",
-    "languages": ["JavaScript", "Python"],
-    "tools": ["Django"]
-  },
-  {
-    "id": 10,
-    "company": "The Air Filter Company",
-    "logo": "./images/the-air-filter-company.svg",
-    "new": false,
-    "featured": false,
-    "position": "Front-end Dev",
-    "role": "Frontend",
-    "level": "Junior",
-    "postedAt": "1mo ago",
-    "contract": "Part Time",
-    "location": "Worldwide",
-    "languages": ["JavaScript"],
-    "tools": ["React", "Sass"]
-  }
-]
+getData();
+//   {
+//     "id": 1,
+//     "company": "Photosnap",
+//     "logo": "./images/photosnap.svg",
+//     "new": true,
+//     "featured": true,
+//     "position": "Senior Frontend Developer",
+//     "role": "Frontend",
+//     "level": "Senior",
+//     "postedAt": "1d ago",
+//     "contract": "Full Time",
+//     "location": "USA Only",
+//     "languages": ["HTML", "CSS", "JavaScript"],
+//     "tools": []
+//   },
+//   {
+//     "id": 2,
+//     "company": "Manage",
+//     "logo": "./images/manage.svg",
+//     "new": true,
+//     "featured": true,
+//     "position": "Fullstack Developer",
+//     "role": "Fullstack",
+//     "level": "Midweight",
+//     "postedAt": "1d ago",
+//     "contract": "Part Time",
+//     "location": "Remote",
+//     "languages": ["Python"],
+//     "tools": ["React"]
+//   },
+//   {
+//     "id": 3,
+//     "company": "Account",
+//     "logo": "./images/account.svg",
+//     "new": true,
+//     "featured": false,
+//     "position": "Junior Frontend Developer",
+//     "role": "Frontend",
+//     "level": "Junior",
+//     "postedAt": "2d ago",
+//     "contract": "Part Time",
+//     "location": "USA Only",
+//     "languages": ["JavaScript"],
+//     "tools": ["React", "Sass"]
+//   },
+//   {
+//     "id": 4,
+//     "company": "MyHome",
+//     "logo": "./images/myhome.svg",
+//     "new": false,
+//     "featured": false,
+//     "position": "Junior Frontend Developer",
+//     "role": "Frontend",
+//     "level": "Junior",
+//     "postedAt": "5d ago",
+//     "contract": "Contract",
+//     "location": "USA Only",
+//     "languages": ["CSS", "JavaScript"],
+//     "tools": []
+//   },
+//   {
+//     "id": 5,
+//     "company": "Loop Studios",
+//     "logo": "./images/loop-studios.svg",
+//     "new": false,
+//     "featured": false,
+//     "position": "Software Engineer",
+//     "role": "Fullstack",
+//     "level": "Midweight",
+//     "postedAt": "1w ago",
+//     "contract": "Full Time",
+//     "location": "Worldwide",
+//     "languages": ["JavaScript", "Ruby"],
+//     "tools": ["Sass"]
+//   },
+//   {
+//     "id": 6,
+//     "company": "FaceIt",
+//     "logo": "./images/faceit.svg",
+//     "new": false,
+//     "featured": false,
+//     "position": "Junior Backend Developer",
+//     "role": "Backend",
+//     "level": "Junior",
+//     "postedAt": "2w ago",
+//     "contract": "Full Time",
+//     "location": "UK Only",
+//     "languages": ["Ruby"],
+//     "tools": ["RoR"]
+//   },
+//   {
+//     "id": 7,
+//     "company": "Shortly",
+//     "logo": "./images/shortly.svg",
+//     "new": false,
+//     "featured": false,
+//     "position": "Junior Developer",
+//     "role": "Frontend",
+//     "level": "Junior",
+//     "postedAt": "2w ago",
+//     "contract": "Full Time",
+//     "location": "Worldwide",
+//     "languages": ["HTML", "JavaScript"],
+//     "tools": ["Sass"]
+//   },
+//   {
+//     "id": 8,
+//     "company": "Insure",
+//     "logo": "./images/insure.svg",
+//     "new": false,
+//     "featured": false,
+//     "position": "Junior Frontend Developer",
+//     "role": "Frontend",
+//     "level": "Junior",
+//     "postedAt": "2w ago",
+//     "contract": "Full Time",
+//     "location": "USA Only",
+//     "languages": ["JavaScript"],
+//     "tools": ["Vue", "Sass"]
+//   },
+//   {
+//     "id": 9,
+//     "company": "Eyecam Co.",
+//     "logo": "./images/eyecam-co.svg",
+//     "new": false,
+//     "featured": false,
+//     "position": "Full Stack Engineer",
+//     "role": "Fullstack",
+//     "level": "Midweight",
+//     "postedAt": "3w ago",
+//     "contract": "Full Time",
+//     "location": "Worldwide",
+//     "languages": ["JavaScript", "Python"],
+//     "tools": ["Django"]
+//   },
+//   {
+//     "id": 10,
+//     "company": "The Air Filter Company",
+//     "logo": "./images/the-air-filter-company.svg",
+//     "new": false,
+//     "featured": false,
+//     "position": "Front-end Dev",
+//     "role": "Frontend",
+//     "level": "Junior",
+//     "postedAt": "1mo ago",
+//     "contract": "Part Time",
+//     "location": "Worldwide",
+//     "languages": ["JavaScript"],
+//     "tools": ["React", "Sass"]
+//   }
+// ]
 //console.log(data.length)
 
+
+// console.log(data1);
 // generateCard();
-displayWithoutFilter()
